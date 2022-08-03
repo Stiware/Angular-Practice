fb708668462b0a85617f280c6713255d85ac112e
stiware <stiwar-quintero@hotmail.com>
2022-08-03 15:20:34 -0500

    se agrega nuevo proyecto sobre pokeapi

---
+++ AngDemoRest/src/app/services/pokemon.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private url: string = "https://pokeapi.co/api/v2/pokemon/";
+  constructor() { }
+
+  //https://youtu.be/lKCWH4Jma8E?t=3900
+  getPokemon(){
+    return
+  }
+}
